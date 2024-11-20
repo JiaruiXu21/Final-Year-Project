8614724c3c8ef59fa340200c9abdfa955d27599d
Jiarui Xu <j.xu@se21.qmul.ac.uk>
2024-11-20 19:38:56 +0800

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Sample route to test
 @app.route('/', methods=['GET'])
